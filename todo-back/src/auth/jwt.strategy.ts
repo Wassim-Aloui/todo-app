ad67aa682c8d4640c04ebfc1476fa330e48fcc94
Wassim-Aloui <61364693+Wassim-Aloui@users.noreply.github.com>
2024-09-19 23:47:38 +0100

    implement register functionality

--- todo-back/src/auth/jwt.strategy.ts
+++ todo-back/src/auth/jwt.strategy.ts
@@ -1,12 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { AuthService } from './auth.service';
 import { jwtConstants } from './constants';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private authService: AuthService) {
+  constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
@@ -14,7 +13,4 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  async validate(payload: any) {
-    return { userId: payload.sub, email: payload.email };
-  }
 }
---
+++ todo-front/src/app/cors/services/todo.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Todo } from '../models/todo.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TodoService {
+  private apiUrl = 'http://localhost:3000/todo';
+
+  constructor(private http: HttpClient) {}
+
+  getTodos(): Observable<Todo[]> {
+    return this.http.get<Todo[]>(this.apiUrl);
+  }
+
+  getTodoById(id: string): Observable<Todo> {
+    return this.http.get<Todo>(`${this.apiUrl}/${id}`);
+  }
+
+  addTodo(todo: Todo): Observable<Todo> {
+    return this.http.post<Todo>(this.apiUrl, todo);
+  }
+
+  updateTodo(id: string, todo: Todo): Observable<Todo> {
+    return this.http.put<Todo>(`${this.apiUrl}/${id}`, todo);
+  }
+
+  deleteTodo(id: string): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+}
